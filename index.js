65a2c536fe67234ea7ad80c5ac654363363fa4cd
dikkar007 <dikkar007@gmail.com>
2023-01-24 20:46:49 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const debug = require('debug')('myapp:server');
+const path = require('path');
+const multer = require('multer');
+const logger = require('morgan');
+const serveIndex = require('serve-index')
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, path.parse(file.originalname).name + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+
+//will be using this for uplading
+const upload = multer({ storage: storage });
+
+//get the router
+//const userRouter =require('./routes/user.route');
+
+app.use(logger('tiny'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'pug');
+//app.use(express.static('public'));
+app.use('/ftp', express.static('public'), serveIndex('public', {'icons': true}));
+
+app.get('/', function(req,res) {
+    res.render('form.pug');
+})
+
+app.post('/upload', upload.single('file'), function(req,res) {
+    debug(req.file);
+    console.log('storage location is ', 'http://'+req.hostname +':5001/' + req.file.path);
+    return res.send('storage location is : http://'+req.hostname +':5001/' + req.file.path);
+})
+
+//if end point is /users/, use the router.
+//app.use('/users', userRouter);
+
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+    debug('Server is up and running on port ', port);
+})
